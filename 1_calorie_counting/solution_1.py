10e18a542bd058eb02295d5c167b5d86894440dd
Sergio Sanchez Zahinos <sersanch@student.42barcelona.com>
2022-12-01 10:50:14 +0100

    prettifyer

--- 1_calorie_counting/solution_1.py
+++ 1_calorie_counting/solution_1.py
@@ -1,14 +1,24 @@
 import sys
 
-if len(sys.argv) != 2:
-    print("Error: Pass one input file as argument.")
-    sys.exit()
+def main():
+    if len(sys.argv) != 2:
+        print("Error: Pass one input file as argument.")
+        sys.exit()
+    inputFile = open(sys.argv[1], "r")
+    data = inputFile.readlines()
+    analyze_elves(data)
 
-#############
-
-inputFile = open(sys.argv[1], "r")
-data = inputFile.readlines()
-
+def analyze_elves(data):
+    elve = 0
+    elves = []
+    elves.append(0)
+    for calories in data:
+        if calories == '\n':
+            elve += 1
+            elves.append(0)
+        else:
+            elves[elve] += int(calories)
+    print("Max calories: {}".format(get_max_cal(elves)))
 
 def get_max_cal(elves):
     max_cal = 0
@@ -17,14 +27,5 @@ def get_max_cal(elves):
             max_cal = elve_cal
     return max_cal
 
-elve = 0
-elves = []
-elves.append(0)
-for calories in data:
-    if calories == '\n':
-        elve += 1
-        elves.append(0)
-    else:
-        elves[elve] += int(calories)
-print("Max calories: {}".format(get_max_cal(elves)))
-
+if __name__ == "__main__":
+    main()
