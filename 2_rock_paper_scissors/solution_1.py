67e88d8dae98295547a57ef5aa62464bfd20c6aa
sszahinos <sergiosaza6@gmail.com>
2022-12-02 09:37:50 +0100

    Solution 1. Commented a version for optimize the result

--- 2_rock_paper_scissors/solution_1.py
+++ 2_rock_paper_scissors/solution_1.py
@@ -13,15 +13,42 @@ def main():
         game_rounds.append(game_round.strip('\n').split())
     print("Max score possible: {}".format(check_game(game_rounds)))
 
-def check_game(data):
-    scores = []
-    scores.append(calc_score(select_game('A', 'B', 'C')))
-    scores.append(calc_score(select_game('A', 'C', 'B')))
-    scores.append(calc_score(select_game('B', 'A', 'C')))
-    scores.append(calc_score(select_game('B', 'C', 'A')))
-    scores.append(calc_score(select_game('C', 'A', 'B')))
-    scores.append(calc_score(select_game('C', 'B', 'A')))
-    return max(scores)
+
+# This version optimizes the selections but is not required
+# def check_game(game_rounds):
+#     scores = [
+#         calc_score(select_game('A', 'B', 'C', game_rounds)),
+#         calc_score(select_game('A', 'C', 'B', game_rounds)),
+#         calc_score(select_game('B', 'A', 'C', game_rounds)),
+#         calc_score(select_game('B', 'C', 'A', game_rounds)),
+#         calc_score(select_game('C', 'A', 'B', game_rounds)),
+#         calc_score(select_game('C', 'B', 'A', game_rounds))
+#     ]
+#     return max(scores)
+def check_game(game_rounds):
+   return (calc_score(select_game(game_rounds)))
+
+
+# This version optimizes the selections but is not required
+# def select_game(x, y, z, game_rounds):
+#     for game_round in game_rounds:
+#         if game_round[1] == 'X':
+#             game_round[1] = x
+#         elif game_round[1] == 'Y':
+#             game_round[1] = y
+#         else:
+#             game_round[1] = z
+#     return game_rounds
+def select_game(game_rounds):
+    for game_round in game_rounds:
+        if game_round[1] == 'X':
+            game_round[1] = 'A'
+        elif game_round[1] == 'Y':
+            game_round[1] = 'B'
+        else:
+            game_round[1] = 'C'
+    return game_rounds
+
 
 def calc_score(game_rounds):
     score = 0
